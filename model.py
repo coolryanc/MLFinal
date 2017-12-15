dff828f2265fb8112865f3c61094043ec0377012
ryanc1993 <ryan149347@gmail.com>
2017-12-16 00:05:42 +0800

    second moidel

--- model.py
+++ model.py
@@ -13,33 +13,34 @@ import parseData
 
 def pair_traing_and_label(trainData, embedding_matrix):
     X_train = []
-    y_train = []
     target_y_train = []
-    for index in range(len(trainData)-2):
+    for index in range(len(trainData)-1):
         if index % 1000 == 0:
             print('\rPairing trainData and label, {}'.format(index), end='', flush=True)
-        tmp = np.concatenate((trainData[index], trainData[index+1]), axis=0)
-        X_train.append(tmp)
-        y_train.append(trainData[index+2])
+        # tmp = np.concatenate((trainData[index], trainData[index+1]), axis=0)
+        target_X_sequence = []
+        for w in trainData[index]:
+            target_X_sequence.append(embedding_matrix[w])
         target_y_sequence = []
-        for w in trainData[index+2]:
+        for w in trainData[index+1]:
             target_y_sequence.append(embedding_matrix[w])
+
+        X_train.append(target_X_sequence)
         target_y_train.append(target_y_sequence)
     print('')
-    return np.array(X_train), np.array(y_train), np.array(target_y_train)
+    return np.array(X_train), np.array(target_y_train)
 
 if __name__ == '__main__':
     train_sequence, word_num, sequence_length, embedding_matrix = parseData.gen_sequence()
-    X_train, y_train, target_y_train = pair_traing_and_label(train_sequence, embedding_matrix) # (676229, 26), (676229, 13)
+    X_train, y_train = pair_traing_and_label(train_sequence, embedding_matrix) # (676229, 26), (676229, 13)
     print(X_train.shape)
     print(y_train.shape)
-    print(target_y_train.shape)
     VALIDATION_SPLIT = 0.1
     EMBEDDING_DIM = 200
-    EPOCH = 25
-    BATCH_SIZE = 64
+    EPOCH = 16
+    BATCH_SIZE = 32
     CELL_SIZE = 256
-    decoder_inputs_dim = target_y_train.shape[2]
+    decoder_inputs_dim = y_train.shape[2]
 
     num_lstm = np.random.randint(175, 275)
     num_dense = np.random.randint(100, 150)
@@ -49,42 +50,39 @@ if __name__ == '__main__':
 
     # teacher forcing
     decoder_target_data = np.zeros(
-        (target_y_train.shape[0], target_y_train.shape[1], target_y_train.shape[2]))
-    for index, sentence in enumerate(target_y_train):
+        (y_train.shape[0], y_train.shape[1], y_train.shape[2]), dtype='float32')
+    for index, sentence in enumerate(y_train):
         if index % 1000 == 0:
             print('\rTeacher forcing, {} data'.format(index), end='', flush=True)
         for t, word in enumerate(sentence):
             if t < len(sentence)-1:
-                decoder_target_data[index][t][:] = target_y_train[index][t+1][:]
+                decoder_target_data[index][t][:] = y_train[index][t+1][:]
 
+    encoder_inputs = Input(shape=(None, EMBEDDING_DIM))
+    encoder = LSTM(CELL_SIZE, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 
-    encoder_inputs = Input(shape=(None,))
-    x = Embedding(word_num, EMBEDDING_DIM, weights=[embedding_matrix], input_length=sequence_length*2, trainable=False)(encoder_inputs)
-    x, state_h, state_c = LSTM(CELL_SIZE,
-                           return_state=True)(x)
     encoder_states = [state_h, state_c]
 
-    decoder_inputs = Input(shape=(None,))
-    x = Embedding(word_num, EMBEDDING_DIM, weights=[embedding_matrix], input_length=sequence_length, trainable=False)(decoder_inputs)
-    x, _, _ = LSTM(CELL_SIZE, return_sequences=True, return_state=True)(x, initial_state=encoder_states)
+    decoder_inputs = Input(shape=(None, decoder_inputs_dim))
+    decoder_lstm = LSTM(CELL_SIZE, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                         initial_state=encoder_states)
+    decoder_dense = Dense(decoder_inputs_dim, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
 
-    decoder_outputs = Dense(decoder_inputs_dim, activation='softmax')(x)
-
-    # Define the model that will turn
-    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
     model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
-    # Compile & run training
+    # Run training
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    model.summary()
-    # plot_model(model, to_file='model.png', show_shapes=True)
-
     earlystopping = EarlyStopping(monitor='val_loss', patience = 10)
     checkpoint = ModelCheckpoint(filepath='./model/currentBest.h5', save_best_only=True, monitor='val_loss')
-    model.fit([X_train, y_train], decoder_target_data,
-              batch_size=BATCH_SIZE,
-              epochs=EPOCH,
-              callbacks=[earlystopping, checkpoint]
+    model.summary()
+    plot_model(model, to_file='newModel.png', show_shapes=True)
+    model.fit([X_train[:128], y_train[:128]], decoder_target_data[:128],
+              batch_size=64,
+              epochs=1,
+              # callbacks=[earlystopping, checkpoint]
               )
     model.save('./model/s2sModel.h5') # save model
 
@@ -96,14 +94,15 @@ if __name__ == '__main__':
     decoder_state_input_c = Input(shape=(CELL_SIZE,))
 
     decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-    e_x = Embedding(word_num, EMBEDDING_DIM, weights=[embedding_matrix], input_length=sequence_length, trainable=False)(decoder_inputs)
-    decoder_outputs, state_h, state_c =LSTM(CELL_SIZE, return_sequences=True, return_state=True)(e_x, initial_state=decoder_states_inputs)
+    decoder_outputs, state_h, state_c = decoder_lstm(
+        decoder_inputs, initial_state=decoder_states_inputs)
 
     decoder_states = [state_h, state_c]
-    decoder_outputs = Dense(decoder_inputs_dim, activation='softmax')(decoder_outputs)
+    decoder_outputs = decoder_dense(decoder_outputs)
     decoder_model = Model(
         [decoder_inputs] + decoder_states_inputs,
         [decoder_outputs] + decoder_states)
+    decoder_model.summary()
     decoder_model.save('./model/s2sDecoder.h5')
-    # plot_model(encoder_model, to_file='encoder_model.png', show_shapes=True)
-    # plot_model(decoder_model, to_file='decoder_model.png', show_shapes=True)
+    plot_model(encoder_model, to_file='new_encoder_model.png', show_shapes=True)
+    plot_model(decoder_model, to_file='new_decoder_model.png', show_shapes=True)
--- predict.py
+++ predict.py
@@ -16,16 +16,22 @@ def seq2Vector(li):
     return np.array(y)
 
 def predict_sequence(encoder_model, decoder_model, input_seq, answer_seq, n_steps=13, cardinality = 200):
-    state = encoder_model.predict(input_seq)
+    v1 = seq2Vector(input_seq)
+    state = encoder_model.predict(v1)
 
-    target_seq = np.array([0.0 for _ in range(13)]).reshape(1, 13)
-    y, h, c = decoder_model.predict([target_seq] + state)
-    y = np.array(y)
+    target_seq = np.zeros((1, 1, cardinality))
+    output = list()
+    for t in range(n_steps):
+        y, h, c = decoder_model.predict([target_seq] + state)
+        output.append(y[0][0])
+        state = [h, c]
+        target_seq = y
+    output = np.array(output)
     _m = sys.maxsize
     ans = 0
     for index, seq in enumerate(answer_seq[0]):
         v = seq2Vector(seq)
-        dis = np.linalg.norm(y - v)
+        dis = np.linalg.norm(output - v)
         if dis < _m:
             _m = dis
             ans = index
@@ -37,16 +43,16 @@ def main(argv):
     test_sequence, answer_sequence = parseData.pad_testing_sequence(rawTestingQuestions, rawTestingAnswers)
     encoder_model = load_model('./model/s2sEncoder.h5')
     decoder_model = load_model('./model/s2sDecoder.h5')
-    writeText = 'id,ans\n'
-    for seq_index in range(len(rawTestingQuestions)):
-        # Take one sequence (part of the training test)
-        # for trying out decoding.
+    fd = open('result1.csv','a')
+    fd.write('id,ans\n')
+    fd.close()
+    for seq_index in range(len(test_sequence)):
         input_seq = test_sequence[seq_index: seq_index + 1]
         answer_seq = answer_sequence[seq_index: seq_index + 1]
         ans = predict_sequence(encoder_model, decoder_model, input_seq, answer_seq)
         print('-')
         print('Input sentence: {} \nANSWER: {} {}'.format("".join(rawTestingQuestions[seq_index]), ans, "".join(rawTestingAnswers[seq_index][ans])))
-        fd = open('result.csv','a')
+        fd = open('result1.csv','a')
         fd.write('{},{}\n'.format(seq_index+1, ans))
         fd.close()
 
